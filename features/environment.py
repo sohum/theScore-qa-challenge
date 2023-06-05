ca7b848ace22553dada733010a19a2d7c9e57e04
sohum <sohum@users.noreply.github.com>
2023-06-04 21:19:43 -0400

    test: Quit driver after each scenario

--- features/environment.py
+++ features/environment.py
@@ -4,16 +4,19 @@ from pages.LeaguePage import LeaguePage
 from pages.LoginPage import LoginPage
 from pages.WelcomePage import WelcomePage
 from pages.components.BottomNav import BottomNav
+from behave import fixture, use_fixture
 
 
-def before_all(context):
+@fixture
+def appium_driver(context):
     context.driver = DriverManager.get_driver()
+    yield context.driver
+    context.driver.quit()
+
+def before_scenario(context, scenario):
+    use_fixture(appium_driver, context)
     context.welcome_page = WelcomePage(context.driver)
     context.login_page = LoginPage(context.driver)
     context.bottom_nav = BottomNav(context.driver)
     context.favorites_page = FavoritesPage(context.driver)
     context.league_page = LeaguePage(context.driver)
-
-
-def after_all(context):
-    context.driver.quit()
